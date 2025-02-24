5cfdbade03bdf9f36422d15dfe1aa9fa094a678f
Harpreet saini <sainii3013@gmail.com>
2025-02-24 15:12:07 +0530

    final commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App";
-import Removed from "./component/removed/Removed";
+import Removed from "./components/removed/Removed"; // ✅ Fixed import path
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Router>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/preview" element={<Removed />} />
-    </Routes>
-  </Router>
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/preview" element={<Removed />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
 );
