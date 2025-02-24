af289b0f8c35a14cf86ca8b56e258a233039f79f
Harpreet saini <sainii3013@gmail.com>
2025-02-24 15:04:31 +0530

    final commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Removed from "./component/removed/Removed";
-import "./index.css"; // Import your Tailwind CSS file
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Router>
