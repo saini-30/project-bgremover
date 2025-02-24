6218ad7f4e35f295fe18d536c4e7f86c080cac26
Harpreet saini <sainii3013@gmail.com>
2025-02-24 14:58:37 +0530

    final commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App";
-import Removed from "./component/removed/removed";
+import Removed from "./component/removed/Removed";
 import "./index.css"; // Import your Tailwind CSS file
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Router>
-  <Routes>
-    <Route path="/" element={<App />} />
-    <Route path="/preview" element={<Removed />} />
-  </Routes>
-</Router>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/preview" element={<Removed />} />
+    </Routes>
+  </Router>
 );
