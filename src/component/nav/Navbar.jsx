32e3842aefc593ed68721520fcb8cebf86548ded
Harpreet saini <sainii3013@gmail.com>
2025-02-24 14:53:19 +0530

    final commit

--- src/component/nav/Navbar.jsx
+++ src/component/nav/Navbar.jsx
@@ -2,20 +2,18 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between p-3 bg-gray-200 fixed w-full z-10">
+    <div className="flex justify-between items-center p-3 bg-gray-200 fixed w-full z-10">
       <div className='px-2'>BGremover</div>
 
-      <div className="flex space-x-40 ">
-       <a href='#'className='hover:bg-gray-500 boder rounded px-2' >About</a>
-       <a href='#'className='hover:bg-gray-500 boder rounded px-2' >services</a>
-       <a href='#'className='hover:bg-gray-500 boder rounded px-2' >Contact</a>
+      <div className="flex space-x-40 items-center">
+        <a href='#' className='hover:bg-gray-500 border rounded px-2'>About</a>
+        <a href='#' className='hover:bg-gray-500 border rounded px-2'>Services</a>
+        <a href='#' className='hover:bg-gray-500 border rounded px-2'>Contact</a>
       </div>
 
-      <div className='px-2 space-x-2'>
-    
-       <button className='border-2 border-gray-700 rounded px-3  hover:bg-gray-500'>login</button>
-        <button className='border-2 border-gray-700 rounded px-3 hover:bg-gray-500 '>signup</button>
-        </div>
+      <div className='px-2 space-x-2 max-w-10  flex items-center'>
+        <img className='size-10 animate-bounce' src="logo.png" alt="logo"></img>
+      </div>
     </div>
   )
 }
