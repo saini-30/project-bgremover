2608aa9a480771198e795582aa3f5c942f5d8355
Harpreet saini <sainii3013@gmail.com>
2025-02-24 15:15:04 +0530

    final commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App";
-import Removed from "./components/removed/Removed"; // ✅ Fixed import path
-import "./index.css";
+import Removed from "./component/removed/Removed";
+import "./index.css"; // Import your Tailwind CSS file
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/preview" element={<Removed />} />
-      </Routes>
-    </Router>
-  </React.StrictMode>
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/preview" element={<Removed />} />
+    </Routes>
+  </Router>
 );
